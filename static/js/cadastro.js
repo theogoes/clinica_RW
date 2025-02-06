aca4ba1d8c834651f79bb1ba330551c10ad1459a
theogoes <theogoes@gmail.com>
2025-02-06 13:46:41 -0300

    bug fix ajax 5

--- static/js/cadastro.js
+++ static/js/cadastro.js
@@ -1,3 +1,5 @@
+ 
+
 const im = document.getElementById("input_im")
 const qtd = document.getElementById("input_qtd")
 const site = document.getElementById("input_site")
@@ -9,27 +11,29 @@ let itens = JSON.parse(localStorage.getItem('inf')) || [];
 
 
 b_cad.addEventListener("click",()=>{
-    let imobiliaria = im.value
-    let salas = qtd.value
-    let link =  site.value
-    let loc = local.value
-    let valor = alor.value
-    const infos = {imobiliaria, salas, link, loc, valor}
+    const infos = {
+        imobiliaria:document.getElementById("input_im").value,
+        local: document.getElementById("input_local").value,
+        valor: document.getElementById("input_valor").value,
+        salas: document.getElementById("input_qtd").value,
+        link: document.getElementById("input_site").value   
+    }
     const json_s = JSON.stringify(infos)
-    itens.push(json_s)
-    saveLocal(itens)
+    
+    saveLocal(json_s)
     saveDB(json_s)
 })
 
 function saveLocal(info){
-    localStorage.setItem("inf",info)
+    const control = Number(localStorage.getItem("inf_control")|| 0) + 1
+    localStorage.setItem("inf_control", control)
+    localStorage.setItem(`inf_${control}`, info)
 }
-
 function saveDB(info){
     $.ajax({
-        url:"/simulacao",
+        url:"/insertdata",
         type:"POST",
-        contentType:"aplication/json",
+        contentType:"application/json",
         data: JSON.stringify(info)
     })
     console.log(info)
