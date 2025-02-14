e792fc84253dac9d337a9f519ab6fbe902790a05
theogoes <theogoes@gmail.com>
2025-02-14 02:19:29 -0300

    front cadastro atualizado. Dinamizado a inserção de textos, desenvolvelos e coloca-los. -> criado paginação

--- static/js/cadastro.js
+++ static/js/cadastro.js
@@ -1,27 +1,29 @@
 const b= document.getElementById("cad_b")
 const txt = document.getElementById("cad_text")
 const i = document.getElementById("cad_i")
+const pg = document.getElementById("pg_txt")
 
 
-const txts = [
-    "",
-    "local",
-    "salas",
-    "valor",
-    "link",
-]
+const txts = ["", "local", "salas", "valor", "link"]
 const campos = ["imobiliaria", "local","salas", "valor","link"]
+const placeHolderTxt = [
+    "",
+    "Insira o bairro do imovel",
+    "Insira a quantiedade de salas do imovel",
+    "Insira o custo mensa (aluguel, pacela, etc..)",
+    "Insira o link de acesso para alugar o imovel"
+]
 const infos = {}
 let count = 0
+
 function teste(){
     b.disabled = true
     count++
     if (count < campos.length) {
         const codValido = validacao(i.value, count)
-        if(codValido == 200){
+        if(codValido === 200){
             if(i.classList.contains("invalid")){
-                i.classList.remove("invalid")
-                i.classList.add("valid")
+                toggleClass(i, "invalid", "valid")
             }
             infos[campos[count - 1]] = i.value 
             tratarTXT(txts[count])
@@ -29,8 +31,8 @@ function teste(){
             tratarValidacao(codValido)
             count--
         }
-        i.value = ""
-        if(count == 4){
+        pg.textContent = `${(count)+1}/${campos.length}`
+        if(count === campos.length - 1){
             b.textContent = "Cadastrar"}
     }else{
         const json_s = JSON.stringify(infos)
@@ -42,17 +44,19 @@ function teste(){
         txt.classList.remove("on")
         txt.classList.add("off")
         setTimeout(()=>{
-            //window.location.replace("http://127.0.0.1:5000/simulacao")
-            window.location.assign("https://psybudget.up.railway.app/simulacao")
+            window.location.replace("http://127.0.0.1:5000/simulacao")
+            //window.location.assign("https://psybudget.up.railway.app/simulacao")
         })
     }      
     console.log(infos);
 }
+
 function saveLocal(info){
     const control = Number(localStorage.getItem("inf_control")|| 0) + 1
     localStorage.setItem("inf_control", control)
     localStorage.setItem(`inf_${control}`, info)
 }
+
 function saveDB(info){
     $.ajax({
         url:"/insertdata",
@@ -62,28 +66,36 @@ function saveDB(info){
     })
     console.log(info)
 }
+
 function validacao(valor, id){
-    console.log(valor +", "+id)
-    if(valor){
-        if(id == 3 || id == 4){
-            if(parseInt(valor)) return 200
-            else return 300
-        }else return 200
-    }else return 301
+    if(!valor) return 301
+    if((id === 3 || id === 4) && !parseInt(valor)) return 300
+    return 200
+    
 }
+
 function tratarValidacao(cod){
-    i.classList.remove("valid")
-    i.classList.add("invalid")
+    toggleClass(i, "valid", "invalid")
     if(cod == 300)tratarTXT("Ultilize apenas números")
     else tratarTXT("Informe um valor")
 }
+
 function tratarTXT(text){
-    txt.classList.remove("on")
-    txt.classList.add("off")
+    toggleClass(txt, "on", "off")
+    toggleClass(i, "on","off")
+    toggleClass(b, "on","off")
     setTimeout(()=>{
         txt.textContent = text
-        txt.classList.remove("off")
-        txt.classList.add("on")
+        i.placeholder = placeHolderTxt[count]
+        toggleClass(txt, "off", "on")
+        toggleClass(i, "off", "on")
+        toggleClass(b, "off", "on")
         b.disabled = false
+        i.value = ""
     },1000)
+}
+
+function toggleClass(input, classOff, clasOn){
+    input.classList.remove(classOff)
+    input.classList.add(clasOn)
 }
