579f347fa413f2799e91ba469225fd1298d953b6
theogoes <theogoes@gmail.com>
2025-02-14 22:53:01 -0300

    cadastro melhorado(possui bug)

--- static/js/cadastro.js
+++ static/js/cadastro.js
@@ -1,11 +1,22 @@
-const b= document.getElementById("cad_b")
-const txt = document.getElementById("cad_text")
-const i = document.getElementById("cad_i")
-const pg = document.getElementById("pg_txt")
+//Elementos DOOM a serem usados
+const cadButtom= document.getElementById("cad_b")   //Botão de cadastro
+const cadText = document.getElementById("cad_text") //Textos apresentados no processo de cadastro
+const cadInput = document.getElementById("cad_i")   //Campo que o usuario preenche
+const cadStep = document.getElementById("pg_txt")   //Paginação do processo de cadastro
 
+//Textos a serem apresentados na tela do usuario
+const cadFrontTexts = ["",
+    "Para iniciarmos, poderia informar o nome da IMOBILIARIA do imovel",
+    "Em qual bairro ele se localiza?", 
+    "Quantas salas (consultórios) ele possui?", 
+    "Valor qual o seu valor mensal?", 
+    "Qual o link da imobiliaria para aluga-lo?",
+    "Cadastro realizado com sucesso! \n Vamos para a simulação"    ]
 
-const txts = ["", "local", "salas", "valor", "link"]
-const campos = ["imobiliaria", "local","salas", "valor","link"]
+//Campos a serem preenchidos para simulação da clinica (usados como chaves no objeto 'infos')
+const fields = ["imobiliaria", "local","salas", "valor","link"]
+
+//Texto a serem apresentados no input, auxiliando o usuario no seu preenchimento
 const placeHolderTxt = [
     "",
     "Insira o bairro do imovel",
@@ -13,44 +24,73 @@ const placeHolderTxt = [
     "Insira o custo mensa (aluguel, pacela, etc..)",
     "Insira o link de acesso para alugar o imovel"
 ]
-const infos = {}
-let count = 0
 
-function teste(){
-    b.disabled = true
-    count++
-    if (count < campos.length) {
-        const codValido = validacao(i.value, count)
-        if(codValido === 200){
-            if(i.classList.contains("invalid")){
-                toggleClass(i, "invalid", "valid")
-            }
-            infos[campos[count - 1]] = i.value 
-            tratarTXT(txts[count])
-        }else{ 
-            tratarValidacao(codValido)
-            count--
-        }
-        pg.textContent = `${(count)+1}/${campos.length}`
-        if(count === campos.length - 1){
-            b.textContent = "Cadastrar"}
-    }else{
-        const json_s = JSON.stringify(infos)
-        saveLocal(json_s)
-        //saveDB(json_s)
-        b.disabled = true
-        b.classList.add("off")
-        i.classList.add("off")
-        txt.classList.remove("on")
-        txt.classList.add("off")
-        setTimeout(()=>{
-            window.location.replace("http://127.0.0.1:5000/simulacao")
-            //window.location.assign("https://psybudget.up.railway.app/simulacao")
-        })
-    }      
+//Objeto a ser construido com as informações preenchidas
+const infos = {}
+
+//Etapa atual do usuario
+let currentStep = 0
+
+/*
+  Função registrationHandler:
+  - Gerencia o fluxo principal do cadastro.
+  - Incrementa a etapa 
+  - Incrementa a etapa e, dependendo do valor de 'currentStep', chama
+    a função de validação ou a função de finalização do cadastro.
+*/
+function registrationHandler(){
+    cadButtom.disabled = true // Desativa o botão para evitar múltiplos cliques
+    currentStep++ // Avança para a próxima etapa
+
+    
+    currentStep < cadFrontTexts.length? // Se ainda há textos para serem mostrados:
+    validationStep() // executa a validação
+    : registrationStep() // caso contrário, finaliza o cadastro.
+
+    cadButtom.disabled = false
     console.log(infos);
 }
 
+function validationStep(){
+    if(currentStep === 1){
+        setTimeout(()=>{toggleClass(cadInput,"off","on")},1000)
+        textChangeAnimation(cadFrontTexts[currentStep])
+        cadStep.textContent = `${(currentStep)+1}/${fields.length}`
+        return
+    }
+    const inputValue = cadInput.value 
+    const validationCod  = validation(inputValue, currentStep)
+
+    if(validationCod !== 200){
+        treatValidation(validationCod)
+        currentStep--
+        return
+    }
+    if(cadInput.classList.contains("invalid"))
+        toggleClass(cadInput, "invalid", "valid")
+
+    infos[fields[currentStep - 2]] = inputValue
+    textChangeAnimation(cadFrontTexts[currentStep])
+    cadStep.textContent = `${(currentStep)}/${fields.length}`
+    if(currentStep === cadFrontTexts.length){
+        toggleClass(cadInput,"on","disable")
+        cadButtom.textContent = "Cadastrar"}
+}
+
+function registrationStep(){
+    const json_s = JSON.stringify(infos)
+    saveLocal(json_s)
+    //saveDB(json_s)
+    cadButtom.disabled = true
+    cadButtom.classList.add("off")
+    cadInput.classList.add("off")
+    cadText.classList.remove("on")
+    cadText.classList.add("off")
+    setTimeout(()=>{
+        window.location.replace("http://127.0.0.1:5000/simulacao")
+        //window.location.assign("https://psybudget.up.railway.app/simulacao")
+    },1000)
+}
 function saveLocal(info){
     const control = Number(localStorage.getItem("inf_control")|| 0) + 1
     localStorage.setItem("inf_control", control)
@@ -67,31 +107,30 @@ function saveDB(info){
     console.log(info)
 }
 
-function validacao(valor, id){
+function validation(valor, id){
     if(!valor) return 301
-    if((id === 3 || id === 4) && !parseInt(valor)) return 300
+    if((fields[currentStep - 2].includes("valor")|| fields[currentStep -2].includes("salas")) && !parseInt(valor)) return 300
     return 200
     
 }
 
-function tratarValidacao(cod){
-    toggleClass(i, "valid", "invalid")
-    if(cod == 300)tratarTXT("Ultilize apenas números")
-    else tratarTXT("Informe um valor")
+function treatValidation(cod){
+    if(cod == 300)textChangeAnimation("Ultilize apenas números")
+    else textChangeAnimation(`Informe um dado para salvarmos como ${fields[currentStep]}`)
+    setTimeout(()=>{toggleClass(cadInput, "valid", "invalid")},1000)
 }
 
-function tratarTXT(text){
-    toggleClass(txt, "on", "off")
-    toggleClass(i, "on","off")
-    toggleClass(b, "on","off")
+function textChangeAnimation(text){
+    toggleClass(cadText, "on", "off")
+    toggleClass(cadInput, "on","off")
+    toggleClass(cadButtom, "on","off")
     setTimeout(()=>{
-        txt.textContent = text
-        i.placeholder = placeHolderTxt[count]
-        toggleClass(txt, "off", "on")
-        toggleClass(i, "off", "on")
-        toggleClass(b, "off", "on")
-        b.disabled = false
-        i.value = ""
+        cadText.textContent = text
+        cadInput.placeholder = placeHolderTxt[currentStep]
+        toggleClass(cadText, "off", "on")
+        toggleClass(cadInput, "off", "on")
+        toggleClass(cadButtom, "off", "on")
+        cadInput.value = ""
     },1000)
 }
 
