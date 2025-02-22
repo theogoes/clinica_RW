d3a8460f7022d83ea06fbbed5345c8e394cfaaa2
theogoes <theogoes@gmail.com>
2025-02-22 16:16:47 -0300

    carroseul movement fix 2

--- static/js/simulacao.js
+++ static/js/simulacao.js
@@ -1,20 +1,13 @@
 
 document.body.onload = buildFront
-var index = 0
-const moveAmout = window.screen.width/2
-var moved = 0
 
 
 function buildFront(){
-    console.log(window.screen.width)
     buildSimulations()
-   nextSimulation(moveAmout) 
-   dinaFocus(index)
-    
-}
+    }
 
 function buildSimulations(){
-    const dinaCar = document.getElementById("simulacao_container")
+    const dinaCar = document.getElementById("simulacao_carroceul")
     let simulacoens = [{}]
     let control = localStorage.getItem("inf_control")
     let step = 1
@@ -27,54 +20,17 @@ function buildSimulations(){
 
         const dinaDiv = document.createElement("div")
         dinaDiv.id = `sim_${index}`
-        dinaDiv.className = "simulacao_content f notOnFocus"
+        dinaDiv.className = "item_carroceul"
         dinaCar.appendChild(dinaDiv)
 
     } )
 }
 
-function dinaFocus(index){
-    const focusDiv = document.getElementById(`sim_${index}`)
-    if(document.getElementById(`sim_${index - 1}`)){
-        document.getElementById(`sim_${index - 1}`).classList.contains("notOnFocus")?
-        document.getElementById(`sim_${index+1}`).classList.add("notOnFocus"):
-        document.getElementById(`sim_${index-1}`).classList.add("notOnFocus")
-    }else{
-        document.getElementById(`sim_${index+1}`).classList.add("notOnFocus")
-    }
-    focusDiv.classList.remove("notOnFocus")
-}
-
-function moveItens(amount){
-    const divsCards = document.querySelectorAll(".simulacao_content")
-    divsCards.forEach(div =>{
-        div.style.transform = `translateX(${amount}px)`
-    })
-}
-
-function nextSimulation(move){
-    index++
-    moved = move + moved
-    dinaFocus(index)
-    moveItens(-moved)
-    
-}
-function previousSimulation(move){
-    index--
-    dinaFocus(index)
-    moved = -moved + move
-    moveItens(moved)
-    console.log(moved)
-    //index === 0 ? moveItens(move/4):moveItens(-move/index)
-
-}
-
-
 
 document.addEventListener("click",function(event){
-    
+    const divsCards = document.querySelectorAll(".item_carroceul")
     let divFocus = event.target.id.split("_")[1]
     if(!isNaN(divFocus)){
-        divFocus < index? previousSimulation(window.screen.width * 0.6107) :nextSimulation(window.screen.width * 0.6107)
+        divFocus < currentIndex? previousSimulation(divsCards) :nextSimulation(divsCards)
     }
 })
