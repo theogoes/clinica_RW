7f16cc05c28dd07b2f06405f3aca06754ab69e0d
theogoes <theogoes@gmail.com>
2025-02-23 13:02:38 -0300

    design slider change -> pegar coisas do localhost

--- static/js/simulacao.js
+++ static/js/simulacao.js
@@ -1,36 +1,21 @@
+let items = document.querySelectorAll(".slider .list .item");
+let thumbs = document.querySelectorAll(".thumbnail .item");
+let countItem = items.length;
+let itemActive = 0;
 
-document.body.onload = buildFront
+thumbs.forEach((thumb, index) => {
+  thumb.addEventListener("click", () => {
+    itemActive = index;
+    showSlider();
+  });
+});
 
+function showSlider() {
+  let itemActiveOld = document.querySelector(".item.active");
+  let itemActiveThumnOld = document.querySelector(".thumbnail .item.active ");
+  itemActiveOld.classList.remove("active");
+  itemActiveThumnOld.classList.remove("active");
 
-function buildFront(){
-    buildSimulations()
-    }
-
-function buildSimulations(){
-    const dinaCar = document.getElementById("simulacao_carroceul")
-    let simulacoens = [{}]
-    let control = localStorage.getItem("inf_control")
-    let step = 1
-    while (step < control) {
-        simulacoens.push(localStorage.getItem(`inf_${step}`))
-        step++
-    }
-
-    simulacoens.forEach(( simulations, index) =>{
-
-        const dinaDiv = document.createElement("div")
-        dinaDiv.id = `sim_${index}`
-        dinaDiv.className = "item_carroceul"
-        dinaCar.appendChild(dinaDiv)
-
-    } )
+  items[itemActive].classList.add("active");
+  thumbs[itemActive].classList.add("active");
 }
-
-
-document.addEventListener("click",function(event){
-    const divsCards = document.querySelectorAll(".item_carroceul")
-    let divFocus = event.target.id.split("_")[1]
-    if(!isNaN(divFocus)){
-        divFocus < currentIndex? previousSimulation(divsCards) :nextSimulation(divsCards)
-    }
-})
